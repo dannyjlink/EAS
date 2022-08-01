c08cacdea01e1d0b6600e23919233dbfe447f0ec
Danny Link <dannyjlink@gmail.com>
2022-08-01 14:58:51 -0500

    Settings going from python to Arduino. TODO parsing.

--- EAS.ino
+++ EAS.ino
@@ -1,57 +1,115 @@
-#include <LiquidCrystal.h>
 #include <SoftwareSerial.h>
+#include <ArduinoJson.h>
 
 /* This file is for use on the mock Edge Auto Switch Demo.  *
  * This is NOT compatible with the PCB Edge Auto Switch.    */
+
+const byte numChars = 175;
+char received[numChars];   // an array to store the received data
+StaticJsonDocument<numChars> settings;
+
+
+/* Bluetooth Module */
+SoftwareSerial bt(11, 10);
+const int state = 9, en = 12;
+
 
 
 enum TEST { BINARY_COUNT, WALKING_1, WALKING_0 };
 
 /* Setup variables for main loop */
-const int monitors[5] = {2, 3, 4, 5, 6};
+
+int monitors[5] = {2, 3, 4, 5, 6};
+
 const bool is_HDMI[5] = {false, false, false, false, true};
-const int start = 7;
-
-/* LCD Related Variables */
-const int rs = A5, en = A4, d4 = A3, d5 = A2, d6 = A1, d7 = A0;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-
 
 /* Default test parameters */
-TEST mode           = BINARY_COUNT;
-int num_monitors    = 5,
-    num_cycles      = 4,
-    low_dwell_time  = 3 * 1000,     // 10 seconds default
-    high_dwell_time = 3 * 1000,     // 30 seconds default
+TEST mode = BINARY_COUNT;
+int num_monitors = 5,
+    num_cycles = 4,
     max = pow(2, num_monitors) - 1; // Pattern where all monitors are on
 
-void setup() {
-    /* Initialize modules */
-    lcd.begin(16, 2);
-    Serial.begin(9600);
+double 
+    low_dwell_time = 3 * 1000,      // 10 seconds default
+    high_dwell_time = 3 * 1000;    // 30 seconds default
     
-    /* Initialize pins */
-    pinMode(start, INPUT);
-    for (int i = 0; i < 5; i++)
-        pinMode(monitors[i], OUTPUT);
+bool received_settings = false,
+     testing           = false;
 
+void setup() {
+    /* Initialize Communications */
+    Serial.begin(9600);
+    Serial.println("Started");
+    bt.begin(9600);
+    bt.println("Ready!!!");
+
+    /* Initialize pins */
+    pinMode(state, INPUT);
+    pinMode(en, OUTPUT);
+    digitalWrite(en, LOW);
+    for (int j = 0; j < 5; j++)
+        pinMode(monitors[j], OUTPUT);
+    all_on();
 }
 
 void loop() {
+    
+    // Wait for bt connection
+    if (!received_settings) {
+        recvWithEndMarker();    // Check if there's data.
+        showreceived_settings();
+        
+        if (received_settings) {      // If there's data, deserialize
+            DeserializationError err = deserializeJson(settings, received);
+            if (err) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(err.f_str());
+            } else {
+                num_monitors = settings["num_monitors"];
+                low_dwell_time = settings["low_dwell"];
+                high_dwell_time = settings["high_dwell"];
+            }
+            delay(100);
+        }
+        
+        
+        
+    }
 
-    all_on();
-    lcd.clear();
-    print_status("Waiting");
 
-    // Wait for button push
-    while (digitalRead(start) == LOW){}
-    lcd.clear();
+
+
+
+
+        /*
+        // Parse settings JSON
+        
+        // Check json was parsed successfully, abort if not.
+        if (err) {
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(err.f_str());
+        } else {
+            num_monitors = settings["num_monitors"];
+            low_dwell_time = settings["low_dwell"];
+            high_dwell_time = settings["high_dwell"];
+            // Remake monitor array
+
+        }
+        */
+        
+       
+
+   
+    // Wait for start signal from bt
+
+
+
 
     /* Testing Logic */
-    for (int cycle = 0; cycle < num_cycles; cycle++) {
-        print_cycle_info(cycle + 1, num_cycles);
-        switch (mode) {
+    
+    else if (testing) {
+        for (int cycle = 0; cycle < num_cycles; cycle++) {
+            switch (mode) {
             case BINARY_COUNT:
                 for (byte pattern = 0; pattern < max; pattern++)
                     test_pattern(pattern);
@@ -64,98 +122,90 @@ void loop() {
                 for (int delta = 1; delta <= max; delta *= 2)
                     test_pattern(max - delta); // Subtract the binary value of the monitor, if second monitor is off: base (11111) - 2^off_monitor (2) = 11101
                 break;
+            }
         }
     }
 }
 
-/* Monitor switch related functions  */
+// Test a pattern and go to high_dwell afterwards
 void test_pattern(byte pattern) {
-    low_dwell(pattern);
-    high_dwell();
-}
-/* Sets monitors to specific testing pattern for specified low_dwell_time */
-void low_dwell(byte pattern) {
-    if (pattern == max) 
-        return;
-    print_status("Low Dwell");
-    print_pattern(pattern);
-    for (int i = 0; i < num_monitors; i++) {
-        if (is_HDMI[i]) {
-            digitalWrite(monitors[i], !bitRead(pattern, i));
+    // Low Dwell
+    if (pattern != max) {
+        for (int i = 0; i < num_monitors; i++) {
+            if (is_HDMI[i])
+                digitalWrite(monitors[i], !bitRead(pattern, i));
+            else
+                digitalWrite(monitors[i], bitRead(pattern, i));
         }
-        else {
-            digitalWrite(monitors[i], bitRead(pattern, i));
-        }
+        delay(low_dwell_time);
     }
-    delay(low_dwell_time);
-}
-/* Sets all monitors to on for specified high_dwell_time */
-void high_dwell() {
-    print_status("High Dwell");
-    print_pattern(byte(max));
+    // High Dwell
     all_on();
     delay(high_dwell_time);
 }
 
-/* LCD Utility functions */
-/* Writes the status to the top right of the LCD Screen */
-void print_status(String status)
-{
-    String msg = status;
-    lcd.setCursor(6, 0);
-    while (msg.length() < 10)
-    {
-        msg += " ";
-    } // Ensure any left over characters are overwritten
-    lcd.print(msg);
-}
-void print_pattern(byte pattern)
-{
-    String labels = "";
-    for (int i = 0; i < num_monitors; i++)
-    {
-        labels += i + 1;
-    }
-    lcd.setCursor(0, 0);
-    lcd.print(labels);
-    lcd.setCursor(0, 1);
-    lcd.print(byte_to_bits(pattern));
-}
-void print_cycle_info(int num, int denom)
-{
-    lcd.setCursor(6, 1);
-    lcd.print(String(num) + "/" + String(denom));
-}
-
 /* Utility */
-String byte_to_bits(byte pattern)
-{
+/* Convert decimal number string to binary number string */
+String byte_to_bits(byte pattern) {
     String p = "";
     for (int i = 0; i < num_monitors; i++)
-    {
         p += bitRead(pattern, i);
-    }
     return p;
+}
+
+double round_to(double val, int precision) {
+    int c = 10 * precision;
+    return (roundf(val * c) / c);
 }
 
 /* Sets all monitors being tested to on */
 void all_on() {
     for (int i = 0; i < num_monitors; i++) {
-        if (is_HDMI[i]) {
+        if (is_HDMI[i])
             digitalWrite(monitors[i], LOW);
-        } else {
+        else
             digitalWrite(monitors[i], HIGH);
-        }
     }
 }
 
 /* Sets all monitors being tested to off */
 void all_off() {
     for (int i = 0; i < num_monitors; i++) {
-        if (is_HDMI[i]) {
+        if (is_HDMI[i])
             digitalWrite(monitors[i], HIGH);
-        } else {
+        else
             digitalWrite(monitors[i], LOW);
+    }
+}
+
+/* Bluetooth functions */
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '#';
+    char rc;
+    
+    while (bt.available() > 0 && received_settings == false) {
+        rc = bt.read();
+
+        if (rc != endMarker) {
+            received[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
         }
+        else {
+            received[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            received_settings = true;
+        }
+    }
+}
+
+void showreceived_settings() {
+    if (received_settings == true) {
+        Serial.print("This just in ... ");
+        Serial.println(received);
+        received_settings = false;
     }
 }
--- client.py
+++ client.py
@@ -1,40 +1,255 @@
-import bluetooth as bt
-import time
+from xmlrpc.client import UNSUPPORTED_ENCODING
+import json
 import serial
+import serial.tools.list_ports
+import os
+import time
 
-name = "HC-05"
-port = 1
-addr = ""
-passkey = "1234"
-done = False
-nearby_devices = bt.discover_devices(lookup_names=True)
-print("Found {} devices.".format(len(nearby_devices)))
-print(nearby_devices)
+# Relative path to preset folder. 
+path = os.path.dirname(__file__)
 
+# Parameter functions
+def integer_input(prompt, min, max):
+    """Takes in user input with prompt & validates based on min and max."""
+    while(True):
+        result = int(input(prompt))
+        if (result < min):
+            print("ERR: Invalid Input -- Must be greater than or equal to", min)
+            continue
+        elif (result > max):
+            print("ERR: Invalid Input -- Must be less than or equal to", max)
+            continue
+        else:
+            return result
 
-for device in nearby_devices:
-    if (device[1] == name):
-        addr = device[0]
+def float_input(prompt, min, max, description):
+    """Takes in user input with prompt & validates based on min and max, returned value is rounded to nearest tenth."""
+    while(True):
+            result = input(prompt)
 
-if (addr == ""):
-    print("Target not found")
-else:
-    print("Target found ", addr)
+            if (result == "D" or result == "d"):
+                print(description)
+            else: 
+                try:
+                    result = float(result)
+                except:
+                    print("ERR: Invalid Input -- Non-numeric input")
+                    continue
 
-try:
-    s = bt.BluetoothSocket(bt.RFCOMM)
-    s.connect((addr, port))
-    print("Connected")
-    s.send("init")
+                if (result < min):
+                    print("ERR: Invalid Input -- Must be greater than or equal to", min)
+
+                elif (result > max):
+                    print("ERR: Invalid Input -- Must be less than or equal to", max)
+                    continue
+                else:
+                    return round(result, 1)
+
+def yes_no_input(prompt, description = "", S_option = True):
+    """Takes in user input with prompt and accepts Y, N, and S, response of S will print description."""
+    while(True):
+        result = input(prompt)
+        if (S_option & (result == "S" or result == "s")):
+            print(description)
+        elif (result == "Y" or result == "y"):
+            return True
+        elif (result == "N" or result == "n"):
+            return False
+        else:
+            print("ERR: Invalid Input")
     
-    while (not done):
-        s.send("test")
-        print("sending")
-        time.sleep(5)
 
 
+def print_dict(obj):
+    result = ""
+    for key in obj:
+        result += key + ": " + str(obj[key]) + "\n"
+    return result
+
+# Bluetooth functions
+def send_json(bt, response):
+    s = json.dumps(response) + "#"
+    bt.write(s.encode())
+    print("SENT:\n" + s)
 
 
-except bt.btcommon.BluetoothError as err:
-    print("Bozo")
-    pass
+def receive(bt):
+    s = (bt.read_all()).decode()
+    print("Received:", s)
+    return s
+
+def get_COM_port():
+    raw_ports = serial.tools.list_ports.comports()
+    ports = [port.device for port in raw_ports]
+    ports.sort()
+
+    options = {}
+    prompt = "Choose COM port assigned to HC05:"
+    for i in range(len(ports)):
+        options[str(i+1)] = ports[i]
+        prompt += " [" + str(i+1) + "] " + ports[i]
+    choice = integer_input(prompt + "\n", 1, len(options))
+    return ports[choice - 1]
+
+# QOL Functions
+def save_settings(obj, filename):
+    try:
+        path = os.path.dirname(__file__) + "\\presets"
+        exists = os.path.exists(path)
+
+        if not exists:  # Make folder if it doesn't exist
+            print("debug: Making folder")
+            os.mkdir(path)
+        
+        # Save file into folder
+        if ".json" not in filename:
+            filename += ".json"
+        path = "./presets/" + filename
+        with open(path, 'w') as fp:
+            json.dump(obj, fp)
+    except:
+        print("Error in saving file.")
+
+def load_settings(filename):
+    path = os.path.dirname(__file__) + "\\presets"
+    exists = os.path.exists(path)
+    if not exists:
+        print("ERR: Preset folder not found, nothing to load\n")
+        return False
+    if ".json" not in filename:
+        filename += ".json"
+    try:
+        f = open(path + "\\" + filename)
+        data = json.load(f)
+        f.close()
+        return data
+    except: 
+        print("Error loading data from file.")
+    
+
+
+# Prompts and descriptions for user input
+prompts = {
+    "load":         "Would you like to use a previously made test preset? [Y]es, [N]o\n",
+    "default":      "Would you like to use the default settings? [Y]es, [N]o, [S]ee Default Settings\n",
+    "test_type":    "What type of test would you like to run?\n[1]Binary Count (default) [2] Walking 1 [3] Walking 0\n",
+    "num_monitors": "How many monitors are you testing? [1-5]\n",
+    "num_cycles":   "How many test cycles would you like? [1-256]\n",
+    "low_dwell":    "How long should the low dwell period be (rounded to nearest 100ms)?\n[0.1-60] seconds (default 10 sec), or [D]escription of low dwell\n",
+    "high_dwell":   "How long should the high dwell period be (rounded to nearest 100ms)?\n[0.1-60] seconds (default 30), or [D]escription of high dwell\n",
+    "ports":        "Since you're using less than 5 monitors, which ports are monitors connected to?\nThe first DP port is 1 & the HDMI port is 5.\nPut a comma and space between each port number. [E]xample\n",
+    "save":         "Do you want to save these settings? [Y]es, [N]o, [S]ee Settings\n"
+}
+
+descriptions = {
+    "low_dwell":    "Low dwell time is how long a test case is applied.\nSay 3 monitors are connected and the test case is [ON, OFF, ON]. Low dwell time is how long that middle monitor is off before all monitors go back to ON (high dwell).\n",
+    "high_dwell":   "High dwell time is how long all monitors are on between test cases.",
+    "ports":        "With three monitors connected, one connected to DP1, another connected to DP2, and the last connected to HDMI, the response would be '1, 2, 5'"
+}
+
+# Object that will be sent to Arduino. Pre-loaded with default settings
+params = {
+    "test_type": 1,
+    "num_monitors": 5,
+    "num_cycles": 5,
+    "low_dwell": 10.0,
+    "high_dwell": 30.0,
+    "ports": [1, 2, 3, 4, 5],
+    "COM": "COM4"
+}
+
+
+#   MAIN PROGRAM START   #
+print("Welcome to Edge Auto Switch")
+print("When prompted for input please respond with whatever is in [brackets] next to the desired option.")
+print("Responses are not case sensitive")
+
+default = False
+load = yes_no_input(prompts["load"], S_option = False)
+
+if load:
+    filename = input("Please enter filename, please do not include the path.")
+
+    loaded = load_settings(filename)
+    if (loaded == False):
+        load = False
+    else:
+        params = loaded
+
+default = False
+if not load:
+    default = yes_no_input(prompts["default"], print_dict(params))  # Determine if operator wants to use defaults
+
+
+if (not default and not load):  # Begin taking in test parameters if new
+    params["COM"]           = get_COM_port()
+    params["test_type"]     = integer_input(prompts["test_type"], 1, 3)
+    params["num_monitors"]  = integer_input(prompts["num_monitors"], 1, 5)
+    params["num_cycles"]    = integer_input(prompts["num_cycles"], 1, 256)
+    params["low_dwell"]     = float_input(prompts["low_dwell"], 0.1, 60.0, descriptions["low_dwell"])
+    params["high_dwell"]    = float_input(prompts["high_dwell"], 0.1, 60.0, descriptions["high_dwell"])
+
+    if (params["num_monitors"] < 5):    # Ports don't need to be assigned if all ports are used
+        while(True):
+            raw_ports = input("Since you're using less than 5 monitors, which ports are monitors connected to?\nThe first DP port is 1 & the HDMI port is 5.\nPut a comma and space between each port number. [E]xample\n")
+            if (raw_ports == "E" or raw_ports == "e"):
+                print(descriptions["ports"])
+            else:
+                raw_ports = raw_ports.split(", ")
+                try:
+                    ports = [int(port) for port in raw_ports]
+                except:
+                    if (len(raw_ports) == 1):
+                        print("ERR: Most likely formatting error in input. Ensure each port number is separated by a comma and space. See [E]xample to see correct formatting.")
+                    continue
+                
+                # Validate port assignments
+                # Check that number of ports is equal to number of monitors
+                if (len(ports) != params["num_monitors"]):
+                    print("Number of monitors does not match number of ports. There should be", params["num_monitors"], "ports input")
+                    continue
+                # Check that there are no duplicates
+                duplicate = False
+                out_of_range = False
+                for i in range(len(ports)):
+                    if (ports[i] < 1 or ports[i] > 5):
+                        out_of_range = True
+                    for j in range(len(ports)):
+                        if (ports[i] == ports[j] and i != j):
+                            duplicate = True
+                
+                if (duplicate):
+                    print("Duplicate port. There can only be one instance of any port.")
+                    continue
+                if (out_of_range):
+                    print("Invalid port number. Ports are numbered from 1 to 5")
+                    continue
+
+                # Put ports in ascending order and set as test parameters
+                ports.sort()
+                params["ports"] = ports
+                break
+
+
+if (not load):
+    save = yes_no_input(prompts["save"], print_dict(params))
+    if (save):
+        filename = input("Enter desired filename.\n")
+        save_settings(params, filename)
+
+print("Parameter input done:")
+print(print_dict(params))
+print()
+
+while True:
+    d = input("Press enter to begin")
+    if (d == ""):
+        bt = serial.Serial(params["COM"], 9600)
+        print("Connected")
+        bt.flushInput()
+        print("Flushed")
+        send_json(bt, params)
+        #time.sleep(5)
+        #bt.close()
+        #os._exit(0)
+        
